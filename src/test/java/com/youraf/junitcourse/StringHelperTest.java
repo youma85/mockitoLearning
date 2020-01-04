05d23618b68ae3431a165f735dc9574197c248d2
Rafii <rafiiyoussef@gmail.com>
2020-01-04 17:50:21 +0100

    initial commit

---
+++ src/test/java/com/youraf/junitcourse/StringHelperTest.java
@@ -0,0 +1,14 @@
+package com.youraf.junitcourse;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StringHelperTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
