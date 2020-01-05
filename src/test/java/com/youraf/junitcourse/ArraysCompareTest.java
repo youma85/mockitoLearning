0453b02e64de1110f48f94f9d9300cbba7717d1d
Rafii <rafiiyoussef@gmail.com>
2020-01-05 08:48:33 +0100

    test performance

--- src/test/java/com/youraf/junitcourse/ArraysCompareTest.java
+++ src/test/java/com/youraf/junitcourse/ArraysCompareTest.java
@@ -22,4 +22,14 @@ public class ArraysCompareTest {
 		int[] numbers= null;
 		Arrays.sort(numbers);
 	}
+	
+	// timeout in millisecond
+	@Test(timeout = 10)
+	public void testArraySort_performance() {
+		int array[]= {12,23,4};
+		for(int i=1;i<1000000;i++) {
+			array[0]=i;
+			Arrays.parallelSort(array);
+		}
+	}
 }
