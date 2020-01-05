cf5362bec2b43116ae3d179116fe2fbee223b03d
Rafii <rafiiyoussef@gmail.com>
2020-01-05 08:21:32 +0100

    Create test method

--- src/test/java/com/youraf/junitcourse/StringHelperTest.java
+++ src/test/java/com/youraf/junitcourse/StringHelperTest.java
@@ -6,9 +6,19 @@ import org.junit.Test;
 
 public class StringHelperTest {
 
+	// test method should be public void
+	// each method should test one condition
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void truncateAInFirst2Position_Condition() {
+		StringHelper hepler=new StringHelper();
+		assertEquals("CD",hepler.truncateAInFirst2Position("AACD"));
 	}
+	
+	@Test
+	public void truncateAInFirst2Position_Condition2() {
+		StringHelper hepler=new StringHelper();
+		assertEquals("CD",hepler.truncateAInFirst2Position("ACD"));
+	}
+
 
 }
