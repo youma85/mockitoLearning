81e8b60ad378e1ee155f4feee27c13477e585f2f
Rafii <rafiiyoussef@gmail.com>
2020-01-05 08:38:07 +0100

    beforeClass & AfterClass

--- src/test/java/com/youraf/junitcourse/BeforeAfterTests.java
+++ src/test/java/com/youraf/junitcourse/BeforeAfterTests.java
@@ -1,12 +1,21 @@
 package com.youraf.junitcourse;
 
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class BeforeAfterTests {
 	
 	StringHelper helper;
+	// method beforeClass should be static
+	// init static objects
+	@BeforeClass
+	public static void beforeClass() {
+		System.out.println("Execute once before");
+	}
+	
 	// will be executed before each method
 	@Before
 	public void setup() {
@@ -27,6 +36,11 @@ public class BeforeAfterTests {
 	@After
 	public void tearDown() {
 		System.out.println("After All tests");
+	}
+	
+	@AfterClass
+	public static void afterClass() {
+		System.out.println("After Class executed once");
 	}
 
 }
