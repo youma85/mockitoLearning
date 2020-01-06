41f248a535f3f9367cc447b45ef87b02650a935a
Rafii <rafiiyoussef@gmail.com>
2020-01-06 09:02:25 +0100

    first mockito code

---
+++ src/test/java/com/youraf/mockito/business/TodoBusinessImplMockTest.java
@@ -0,0 +1,54 @@
+package com.youraf.mockito.business;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+import com.youraf.mockito.data.api.TodoService;
+
+public class TodoBusinessImplMockTest {
+
+	@Test
+	public void retrieveTodosRelatedToSpring_usingMock() {
+		// create a mock object of todoService
+		TodoService todoServiceMock = mock(TodoService.class);
+		
+		List<String> todos=Arrays.asList("Learn Spring MVC", "Learn Spring",
+				"Learn Angular");
+		
+		// when call the method retrieve with this parameter return back a todoList
+		// Enables stubbing methods. Use it when you want the mock to return particular value when particular method is called.
+		when(todoServiceMock.retrieveTodos("Youraf"))
+		.thenReturn(todos);
+		
+		
+		TodoBusinessImpl todoBusinessImpl = new TodoBusinessImpl(todoServiceMock);
+		List<String> filteredTodos = todoBusinessImpl
+				.retrieveTodosRelatedToSpring("Youraf");
+		assertEquals(2, filteredTodos.size());
+	}
+
+	@Test
+	public void retrieveTodosRelatedToSpring_usingMockWithEmptyList() {
+		// create a mock object of todoService
+		TodoService todoServiceMock = mock(TodoService.class);
+		
+		List<String> todos=Arrays.asList();
+		
+		// when call the method retrieve with this parameter return back a todoList
+		when(todoServiceMock.retrieveTodos("Youraf"))
+		.thenReturn(todos);
+		
+		
+		TodoBusinessImpl todoBusinessImpl = new TodoBusinessImpl(todoServiceMock);
+		List<String> filteredTodos = todoBusinessImpl
+				.retrieveTodosRelatedToSpring("Youraf");
+		assertEquals(0, filteredTodos.size());
+	}
+
+}
