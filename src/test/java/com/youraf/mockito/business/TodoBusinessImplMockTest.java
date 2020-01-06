62853c8f50f38428a0329d8d075558516363fdc4
Rafii <rafiiyoussef@gmail.com>
2020-01-06 11:13:28 +0100

    Captured an argument

--- src/test/java/com/youraf/mockito/business/TodoBusinessImplMockTest.java
+++ src/test/java/com/youraf/mockito/business/TodoBusinessImplMockTest.java
@@ -4,6 +4,7 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
@@ -15,6 +16,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 
 import com.youraf.mockito.data.api.TodoService;
 
@@ -95,9 +97,11 @@ public class TodoBusinessImplMockTest {
 		//verify method will check if some method is called
 		// here only LEarn angular didn't contain Spring 
 		verify(todoServiceMock).deleteTodo("Learn Angular");
+		// or by then(todoServiceMock).should().deleteTodo("Learn Angular");
 		
 		// verify if a method never called
 		verify(todoServiceMock,never()).deleteTodo("Learn Spring MVC");
+		// then(todoServiceMock).should(never()).deleteTodo("Learn Spring MVC");
 		
 		// verify if a method is called n times
 		verify(todoServiceMock,times(1)).deleteTodo("Learn Angular");
@@ -105,5 +109,34 @@ public class TodoBusinessImplMockTest {
 		// verify if a method is called at least once
 		verify(todoServiceMock,atLeastOnce()).deleteTodo("Learn Angular");
 	}
+	
+	@Test
+	public void deleteTodosNotRelatedToSpring_capturingArgumentTwice() {
+		// declare argument captor
+		ArgumentCaptor<String> stringArgumentCaptor=ArgumentCaptor.forClass(String.class);
+		
+		// define argument captor on specific method call
+		
+		
+		
+		// Given : Setup
+		TodoService todoServiceMock = mock(TodoService.class);
+		
+		List<String> todos=Arrays.asList("Learn Java", "Learn Spring",
+				"Learn Angular");
+		
+		given(todoServiceMock.retrieveTodos("Youraf")).willReturn(todos);
+		
+		TodoBusinessImpl todoBusinessImpl = new TodoBusinessImpl(todoServiceMock);
+		// When : method call
+		todoBusinessImpl.deleteTodosNotRelatedToSpring("Youraf");
+
+		// Then : asserts
+		// here  LEarn angular and Learn java will be deleted 
+		// capture the arguments of
+		then(todoServiceMock).should(times(2)).deleteTodo(stringArgumentCaptor.capture());
+		assertThat(stringArgumentCaptor.getAllValues().size(), is(2) );
+	}
+
 
 }
