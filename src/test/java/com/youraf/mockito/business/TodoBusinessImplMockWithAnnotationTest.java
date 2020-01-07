a03187f6f1c72a33299b1e3a743b65b11ed9655a
Rafii <rafiiyoussef@gmail.com>
2020-01-07 10:45:21 +0100

    Junit rule

--- src/test/java/com/youraf/mockito/business/TodoBusinessImplMockWithAnnotationTest.java
+++ src/test/java/com/youraf/mockito/business/TodoBusinessImplMockWithAnnotationTest.java
@@ -15,18 +15,24 @@ import static org.mockito.Mockito.when;
 import java.util.Arrays;
 import java.util.List;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import com.youraf.mockito.data.api.TodoService;
 
-@RunWith(MockitoJUnitRunner.class)
+//@RunWith(MockitoJUnitRunner.class)
 public class TodoBusinessImplMockWithAnnotationTest {
+	
+	@Rule
+	public MockitoRule mockitoRule= MockitoJUnit.rule();
 	
 	// creates a mock for the todoService instead of using the mock() function
 	// need to use @RunWith(MockitoJUnitRunner.class)
